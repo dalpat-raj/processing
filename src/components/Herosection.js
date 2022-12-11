bbf3a28fbf0090293dbd41af6edcdb5b9768fe3b
mahendrameghwal <solankirakesh895@gmail.com>
2022-12-11 16:04:13 +0530

    okok

---
+++ src/components/Herosection.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import carousel1 from '../img/carousel-1.jpg'
+import carousel2 from '../img/carousel-2.jpg'
+import carousel3 from '../img/carousel-3.jpg'
+
+const Herosection =()=>{
+
+
+
+    return(
+        <div className="container hero__section">
+         <div className="row">
+            <div className="col-1">
+                <Carousel autoPlay={true} infiniteLoop={true} className="carousel">
+                <div className="col">
+                    <img src={carousel1}/>
+                    <p>lorem dalpat</p>
+                    </div>
+                    <div className="col">
+                    <img src={carousel2} />
+                    <p>lorem dalpat</p>
+                    </div>
+                    <div className="col">
+                    <img src={carousel3} />
+                    <p>lorem dalpat</p>
+                </div>
+                </Carousel>
+            </div>
+
+            <div className="col-2">
+
+            </div>
+         </div>
+        </div>
+    )
+}
+
+export default Herosection;
